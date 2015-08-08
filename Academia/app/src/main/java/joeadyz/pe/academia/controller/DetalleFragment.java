d7a52a33a821908bc3fbf46486ee8a5fe00587fa
Jose Diaz <jose.diaz@joedayz.pe>
2015-08-08 16:07:43 -0500

    pantalla de detalle

--- Academia/app/src/main/java/joeadyz/pe/academia/controller/DetalleFragment.java
+++ Academia/app/src/main/java/joeadyz/pe/academia/controller/DetalleFragment.java
@@ -5,8 +5,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import joeadyz.pe.academia.R;
+import joeadyz.pe.academia.modelo.Prueba;
 
 
 /**
@@ -14,10 +18,37 @@ import joeadyz.pe.academia.R;
  */
 public class DetalleFragment extends Fragment {
 
+    private Prueba prueba;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View listaPruebas = inflater.inflate(R.layout.prueba_detalle, null);
+        View layout = inflater.inflate(R.layout.prueba_detalle, null);
+
+        if(getArguments()!=null){
+            this.prueba = (Prueba) getArguments().getSerializable("prueba");
+        }
+
+        if(this.prueba!=null){
+            TextView materia = (TextView)layout.findViewById(R.id.detalle_prueba_materia);
+            TextView fecha = (TextView)layout.findViewById(R.id.detalle_prueba_fecha);
+            ListView topicos = (ListView)layout.findViewById(R.id.detalle_prueba_topicos);
+
+            materia.setText(this.prueba.getMateria());
+            fecha.setText(this.prueba.getFecha());
+
+            ArrayAdapter<String> adapter =
+                    new ArrayAdapter<String>(
+                            getActivity(),
+                            android.R.layout.simple_list_item_1,
+                            prueba.getTopicos());
+
+            topicos.setAdapter(adapter);
+            return layout;
+        }
+
+
+
 
         return super.onCreateView(inflater, container, savedInstanceState);
     }
